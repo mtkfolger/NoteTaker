2d1a7d19efe3a0af2285f809a5cd718d6a8a8de4
Michael Folger <folger@zoller-usa.com>
2021-08-15 15:36:45 -0500

    updated server.js

--- Assets/Develop/public/server.js
+++ Assets/Develop/public/server.js
@@ -1,17 +1,23 @@
 // Import express package
 const express = require('express');
 
-// Require the JSON file and assign it to a variable called `termData`
-const termData = require('./terms.json');
-const PORT = 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
 
+//Set the port to listen on
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+
 app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api', (req, res) => res.json(termData));
+app.get('/api', (req, res) => res.json(noteData));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
